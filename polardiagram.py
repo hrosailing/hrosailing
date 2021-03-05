cbbe74c180f568e5cb5953f96739ce91b7e87839
VFDannenberg <71141828+VFDannenberg@users.noreply.github.com>
2021-03-05 13:56:58 +0100

    Add files via upload

--- polardiagram.py
+++ polardiagram.py
@@ -1,69 +1,91 @@
 import numpy as np
+import csv
+import sqlite3
 import pandas as pd
 import matplotlib.pyplot as plt
-import pynmea2
-import sqlite3
 
 import logging
 logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.DEBUG)
 
-# V: Algorithmus zum Berechnen der konvexen Hülle einer 2d-Punkt-Wolke
-# Gibt Liste der Ecken aus
-# Muss aber noch für die Zwecke hier bearbeitet werden -> Ich denke an eine interne Konvertierung von
-# Polar- zu kartesischen Koordinaten, die sich aber nicht auf die Punkte auswirken soll...
-#def convex_hull_2d(point_cloud):
-#    point_cloud = sorted(set(point_cloud))
-#    if len(point_cloud) <= 1:
-#        return point_cloud
+# V: Algorithmus zum Berechnen der konvexen Hülle einer 2d-Punkt-Wolke (mit Punkten in Polarkoordinaten!)
+# Muss noch bearbeitet werden
+def convex_hull_2d(point_cloud):
+    point_cloud = sorted(set(point_cloud))
+    if len(point_cloud) <= 1:
+        return point_cloud
     
-#    def cross(o, a, b):
-#        return (a[0] - o[0]) * (b[1] - o[1]) - (a[1] - o[1]) * (b[0] - o[0])
+    def cross(o, a, b):
+        return (a[0] - o[0]) * (b[1] - o[1]) - (a[1] - o[1]) * (b[0] - o[0])
     
-#    lower_hull_list = []
-#    for point in point_cloud:
-#        while len(lower_hull_list) >= 2 and cross(lower_hull_list[-2], lower_hull_list[-1],point) <= 0:
-#            lower_hull_list.pop()
-#        lower_hull_list.append(point)
+    def polar_to_kartesian(point):
+        return [point[0] * np.cos(point[1]), point[0] * np.sin(point[1])]
         
-#    upper_hull_list = []
-#    for point in reversed(point_cloud):
-#        while len(upper_hull_list) >= 2 and cross(upper_hull_list[-2], lower_hull_list[-1], point) <= 0:
-#            upper_hull_list.pop()
-#        upper_hull_list.append(point)
+    lower_hull_list = []
+    for point in point_cloud:
+        while len(lower_hull_list) >= 2 and cross(polar_to_kartesian(lower_hull_list[-2]),                                                                                         polar_to_kartesian(lower_hull_list[-1]),
+                                                  polar_to_kartesian(point)) <= 0:
+            lower_hull_list.pop()
+        lower_hull_list.append(point)
+        
+    upper_hull_list = []
+    for point in reversed(point_cloud):
+        while len(upper_hull_list) >= 2 and cross(polar_to_kartesian(upper_hull_list[-2]),                                                                                         polar_to_kartesian(lower_hull_list[-1]), 
+                                                  polar_to_kartesian(point)) <= 0:
+            upper_hull_list.pop()
+        upper_hull_list.append(point)
     
-#    return lower_hull_list[:-1] + upper_hull_list[:-1]
+    return lower_hull_list[:-1] + upper_hull_list[:-1]
     
+
 
 # V: Statt von einer csv-Datei zu lesen, kann man soweit ich gesehen habe auch gleich die NMEA-Datei als solche einlesen
-# Hat der Stefan Simon glaub ich auch gemacht
 # Dabei kann man wahrscheinlich auch besser kontrollieren, welche Werte man mitnehmen will
-def from_nmea(nmea_path):
+#def from_nmea(nmea_path):
     
     
     
+
+#def from_pol(pol_path):
     
-    
+def convert_apparent_wind_angle_to_true(data):
+    #true_wind_angle = arcsin(true_wind_speed * sin(apparent_wind_angle) / apparent_wind_speed)?
+       
 def convert_apparent_wind_speed_to_true(data):
-    #true_wind_speed = apparent_wind_speed * sin(true_wind_angle) / sin(apparent_wind_angle)
-    
-#def convert_apparent_wind_angle_to_true(data):
+    #true_wind_speed = apparent_wind_speed - speed_over_ground? (wenn course_over_ground = apparent_wind_direction?)
     
 
 
-class PolarDiagramTable:
+
+class PolarDiagramTable():
     def __init__(self,data = np.zeros((72,20))):
-        self.data = data
+        self.__data = data
         #self.plot = None
-   
-    def _convert_to_table(self):
-        return self.data
+    
+    def set_data(self, data, true_wind_speed = None, true_wind_angle = None):
+        if true_wind_speed == None and true_wind_angle == None:
+            self.__data = data
+        elif true_wind_speed == None:
+            self.__data[true_wind_angle,:] = data
+        elif true_wind_angle == None:
+            self.__data[:,true_wind_speed] = data
+        else:
+            self.__data[true_wind_angle, true_wind_speed] = data
+            
         
-    def _plot_slice(self,true_wind_speed):
+    
+    def get_data(self):
+        return self.__data
+    
+    
+    def convert_to_table(self):
+        return
+        
+    def plot_slice(self,true_wind_speed):
         true_wind_angles = [np.radians(a) for a in np.arange(0,360,5)]
         # V: Hier müsste vielleicht noch nen Error abgefangen werden, wenn true_wind_speed ungerade ist
         # oder man macht die Tabelle so, dass sie true_wind_speed in 1er-Schritten dokumentiert,
         # obwohl sich wahrscheinlich bei einem Knoten Unterschied nicht viel ändert?
-        boat_speed = self.data[:,true_wind_speed/2]
+        boat_speed = self.__data[:,true_wind_speed/2]
         plot_diagram = plt.subplot(1,1,1, projection = 'polar')
         # V: Polarplot hat standardmäßig 0° nach Osten und geht gegen den Uhrzeigersinn
         # Setzen 0° auf Norden und gehen im Uhrzeigersinn.
@@ -71,62 +93,49 @@ class PolarDiagramTable:
         plot_diagram.set_theta_direction('clockwise')
         plot_diagram.plot(true_wind_angles, boat_speed, 'ro-', ms = 1, lw = 0.75)
         plt.show()
-        # V: Man könnte hier vielleicht zusätzlich den Plot als eine Eigenschaft der Klasse sehen
-        # das müsste man dann oben noch setzen, sodass man dann mit .plot oder so darauf zugreifen kann.
-        # Dabei müsste man aber auch wieder die Windgeschwindigkeit welche man geplottet haben möchte noch
-        # angeben, also eher sowas wie .plot(true_wind_speed).
-        # Das könnte vielleicht Aufwand sparen, wenn wir das ganze nicht mehr nur "diskret" plotten wollen,
-        # sondern durch die Tabelle eine Art Approximation an das "kontinuierliche" Polardiagram kriegen wollen,
-        # was ja möglicherweise immer dauern kann, sowas zu plotten.
+        # V: Man könnte hier vielleicht zusätzlich die Plots als eine Property der Klasse verwenden?
+        # das müsste man dann oben noch setzen, sodass man dann mit .plot[...] oder so darauf zugreifen kann.
         # Ich hab aber auch keine Ahnung, ob sowas möglich ist, weil man die Plots dafür ja irgendwie
-        # speichern muss.
+        # speichern muss?
     
-    def _plot_3d(self):
+    def plot_3d(self):
         # V: Erstmal nur das Konzept einer Funktion, um eine 3d-Darstellung der Slices, beziehungsweise
         # auch, wenn möglich, der konvexen Hülle zu bekommen?
     
-    
-    def _convex_hull_slice(self, true_wind_speed):
-        # V: Hier soll die konvexe Hülle eines Slices des Polardiagrams berechnet werden -> für _find_cross_course
+    def convex_hull_slice(self, true_wind_speed):
+        # V: Hier wird die konvexe Hülle eines Slices des Polardiagrams berechnet -> für find_cross_course
         # Kann vielleicht auch komplett rausgelassen werden und durch einfaches Aufrufen der convex_hull_2d - Funktion
-        # erledigt werden?
-        # Kann aber möglicherweise zur Anschaulichkeit der konvexen Hülle dienen? (siehe unten)
-        convex_hull = convex_hull_2d(self.data[true_wind_speed/2])
+        # erledigt werden, allerdings stellt die Funktion die Hülle auch dar (Plot)
+        points = tuple([(self.__data[int(a/5), true_wind_speed], a) for a in np.arange(0,360,5)])
+        convex_hull = convex_hull_2d(points.copy())
+        true_wind_angles = [np.radians(point[1]) for point in convex_hull]
+        boat_speed = [point[0] for point in convex_hull]
         plot_convex_hull = plt.subplot(1,1,1, projection = 'polar')
         plot_convex_hull.set_theta_zero_location('N')
         plot_convex_hull.set_theta_direction('clockwise')
-        plot_convex_hull.plot( , , 'ro-', ms = 1, lw = 0.75)
+        # V: Der Plot hat noch ein paar Macken .... 
+        plot_convex_hull.plot(true_wind_angles, boat_speed, 'ro-', ms = 1, lw = 0.75)
         plt.show()
-        
+        return convex_hull
     
-    # V: Funktion um zu einem gegebenen true_wind_speed und true_wind_angle/Richtung wo man hinwill, einen 
-    # Kreuz-kurs zu berechnen
-    def _find_cross_course(self,true_wind_speed,true_wind_angle):
-        convex_hull = convex_hull_2d(self.data[true_wind_speed/2])
-        
-        # Schnittpunkt mit konvexer Hülle finden
-        # Konvexkombination der Ecken berechnen
-        # Daraus Kurs angeben
-        
-        
-        
+    def convex_hull(self):
+        # V: Funktion zum Plotten der konvexen des 3d-Polardiagrams
+ 
+
+    def find_cross_course(self,true_wind_speed,true_wind_angle):
         
     
     
     
 class PolarDiagramParam(PolarDiagramTable):
-    # V: Hier hab ich absolut keine Ahnung wie man sowas abspeichern könnte....?
-    # Außer vielleicht als plot..., aber da müsste man ja auch irgendwie eine Möglichkeit habe
-    # die Daten daraus automatisch zu extrahieren, damit man das überhaupt in eine Tabelle
-    # konvertieren kann
-    def __init__(self):
-        self.data =
+    # V: Hier hab ich absolut keine Ahnung wie man sowas abspeichern könnte....? -> plot(s)?? Funktion(en)??
+    def __init__(self, data = ...):
+        self.__data =
 
-    def _convert_to_table(self):
-        # V: Hier sollen Interpolationsmethoden benutzt werden, um Daten an den
-        # TWAs in jeweils 5er Schritten (0°,5°,10°,15°,......,355°) zu erhalten
-        # und da Objekt dann zu einem PolarDiagramTable-Objekt zu konvertieren
-        # V: --> Ergibt aber nur eine Spalte der Tabelle, da Windgeschwindigkeit "konstant"?
+    def convert_to_table(self):
+        # V: Hier kann man wahrscheinlich an den jeweiligen Punkten dann ablesen und daraus
+        # EINE Spalte von so einer Tabelle erhalten, es sei denn wir machen dass so, dass man
+        # mehrere parametrisierte(?) Darstellungen hat?
 
 
 
@@ -135,21 +144,25 @@ class PolarDiagramPointCloud(PolarDiagramTable):
         # Ich bin noch unentschlossen, ob man hier nicht auf einfach ein np.array verwenden sollte,
         # ähnlich wie bei PolarDiagramTable, oder ob man bei beiden ein pandas Dataframe verwendet,
         # obwohl das bei der Table-Sache glaub ich Blödsinn ist?
-        self.data = pd.Dataframe(data = data , columns = ['TWS', 'TWA', 'BSP'])
-        # V: So wäre Punktwolke "3-dimensional"......
-        # Muss man glaub ich aber auch so machen, da sonst Konvertierung zu Tabelle schwer wird? (siehe unten)
+        self.__data = pd.Dataframe(data = data , columns = ['TWS', 'TWA', 'BSP'])
+    
+    def plot_3d(self):
+        # V: Funktion zum 3d-plotten der Punktwolke.
+        # Noch in Bearbeitung
         
     
-    def _convert_to_table(self):
-        # V: Hier sollen Interpolationsmethoden benutzt werden, um Daten an den
+    def convert_to_table(self):
+        # V: Hier sollen Interpolationsmethoden/Machine Learning benutzt werden, um Daten an den
         # TWAs in jeweils 5er Schritten (0°,5°,10°,15°,......,355°) zu erhalten
         # und das Objekt dann zu einem PolarDiagramTable-Objekt zu konvertieren?
+        # -> Forschung!
         # V: --> Ergibt aber nur eine Spalte der Tabelle, da Windgeschwindigkeit "konstant"?
         
         
-    def _convert_to_param(self):
+    def convert_to_param(self):
         # V: Hier könnte man versuchen, aus der Punktwolke ( oder auch nur aus einem Slice der Punktwolke ) eine
         # parametrisierte Darstellung zu berechnen?
+        # -> Forschung?
         
         
     
