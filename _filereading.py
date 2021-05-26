def5bfd7b5523a72b085f2b6ebdf3005154547df
VFDannenberg <71141828+VFDannenberg@users.noreply.github.com>
2021-05-26 09:52:12 +0200

    Add files via upload

--- _filereading.py
+++ _filereading.py
@@ -1,5 +1,6 @@
 import numpy as np
 import csv
+import pynmea2
 
 
 # V: In Arbeit
@@ -33,7 +34,7 @@ def read_extern_format(csv_path, fmt):
 
     if fmt == 'orc':
         delimiter = ';'
-    if fmt == 'opencpn':
+    else:
         delimiter = ','
 
     return read_sail_csv(csv_path, delimiter)
@@ -57,3 +58,31 @@ def read_sail_csv(csv_path, delimiter):
 def read_array_csv(csv_path):
     file_data = np.genfromtxt(csv_path, delimiter="\t")
     return file_data[0, 1:], file_data[1:, 0], file_data[1:, 1:]
+
+
+def read_nmea_file(nmea_path):
+    with open(nmea_path, 'r') as nmea_file:
+        nmea_vtg_sentences = filter(
+            lambda line: "$GPVTG" in line,
+            nmea_file)
+        nmea_wimwv_sentences = filter(
+            lambda line: "$WIMWV" in line,
+            nmea_file
+        )
+        nmea_data = []
+        vtg = next(nmea_vtg_sentences)
+        bsp1 = float(pynmea2.parse(vtg).spd_over_grnd_kts)
+
+        for vtg in nmea_vtg_sentences:
+            bsp2 = float(pynmea2.parse(vtg).spd_over_grnd_kts)
+            for i in range(10):
+                wimwv = pynmea2.parse(
+                    next(nmea_wimwv_sentences))
+                wa = float(wimwv.wind_angle)
+                ws = float(wimwv.wind_speed)
+                bsp = (9-i)/9 * bsp1 + i/9 * bsp2
+                nmea_data.append((ws, wa, bsp, wimwv.reference))
+
+            bsp1 = bsp2
+
+    return nmea_data
