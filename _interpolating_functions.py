bdcfb1ac8fb2c3ca6c1990fcda915c9e77d0fcb9
VFDannenberg <71141828+VFDannenberg@users.noreply.github.com>
2021-05-28 12:19:08 +0200

    Add files via upload

--- _interpolating_functions.py
+++ _interpolating_functions.py
@@ -2,50 +2,52 @@ import numpy as np
 
 
 def interpolate_grid(w_res, w_points, neighbourhood=None,
-                     eval_func=None, radius=1, alpha=1,
-                     beta=1, **kwargs):
-    ws_res, wa_res = w_res
-    ws_res, wa_res = np.meshgrid(ws_res, np.deg2rad(wa_res),
-                                 indexing='ij')
+                     eval_func=None, **kwargs):
 
     if neighbourhood is None:
         neighbourhood = ball_neighbourhood
 
-    kwargs["radius"] = radius
-
     if eval_func is None:
-        eval_func = gauss_potential
+        eval_func = weighted_arithm_mean
 
-    kwargs["alpha"] = alpha
-    kwargs["beta"] = beta
-
-    return interpolate_grid_points(ws_res, wa_res, w_points,
+    return interpolate_grid_points(w_res, w_points,
                                    neighbourhood, eval_func,
                                    **kwargs)
 
 
-def interpolate_grid_points(ws_res, wa_res, w_points,
+def interpolate_grid_points(w_res, w_points,
                             neighbourhood, eval_func,
                             **kwargs):
+    ws_res, wa_res = w_res
     data = np.zeros((len(wa_res), len(ws_res)))
     for i in range(len(ws_res)):
         for j in range(len(wa_res)):
-            grid_point = np.array([ws_res[j, i], wa_res[j, i]])
+            grid_point = np.array([ws_res[i], wa_res[j]])
             dist, mask = neighbourhood(
-                w_points.points[:, :2] - grid_point, kwargs["radius"])
-            dist = dist[mask]
-            weights = w_points.weights[mask]
-            data[j, i] = eval_func(dist, weights, **kwargs)
+                w_points.points[:, :2] - grid_point, **kwargs)
+            data[j, i] = eval_func(
+                w_points.points[mask], dist[mask],
+                w_points.weights[mask], **kwargs)
+
+    print(data)
 
     return data
 
 
-def ball_neighbourhood(vec, radius):
-    distance = np.sum(np.square(vec, axis=1), axis=1)
+def ball_neighbourhood(vec, **kwargs):
+    radius = kwargs.get('radius', 1)
+    vec[:, 1] = np.deg2rad(vec[:, 1])
+    distance = np.linalg.norm(vec, axis=1)
     return distance, distance <= radius
 
 
-def gauss_potential(dist, weight, **kwargs):
-    alpha = kwargs.get("alpha")
-    beta = kwargs.get("beta")
-    return np.sum(beta * np.exp(-alpha * np.norm(weight * dist)))
+def weighted_arithm_mean(points, weights, dist, **kwargs):
+    alpha = kwargs.get('alpha', 1)
+    beta = kwargs.get('beta', 1)
+    weights = gauss_potential(dist, weights, alpha, beta)
+    scal_fac = kwargs.get('s', 1)
+    return scal_fac * weights @ points[:, 2] / len(weights)
+
+
+def gauss_potential(dist, weights, alpha, beta):
+    return beta * np.exp(-alpha * weights * dist)
