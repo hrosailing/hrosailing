98595551a89c84c4c722288ae0d2b7ac9a4bd003
VFDannenberg <71141828+VFDannenberg@users.noreply.github.com>
2021-06-25 16:33:14 +0200

    Add files via upload

---
+++ hrosailing/polardiagram/__init__.py
@@ -0,0 +1,13 @@
+
+
+from polardiagram.polardiagram import (
+    PolarDiagram,
+    PolarDiagramTable,
+    PolarDiagramPointcloud,
+    PolarDiagramCurve,
+    from_csv,
+    to_csv,
+    pickling,
+    depickling,
+    symmetric_polar_diagram,
+)
