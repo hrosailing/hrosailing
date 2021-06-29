86da6a039f86911ab4334905f7cf81917b2426d6
VFDannenberg <71141828+VFDannenberg@users.noreply.github.com>
2021-06-29 15:26:13 +0200

    Add files via upload

---
+++ tests/__init__.py
@@ -0,0 +1,19 @@
+
+
+from tests.polardiagramtests import (
+    polar_table_suite,
+    point_cloud_suite,
+    polar_curve_suite
+)
+
+
+from tests.utilstest import (
+    speed_resolution_suite,
+    angle_resolution_suite,
+)
+
+
+from tests.windconversiontests import (
+    apparent_wind_suite,
+    true_wind_suite,
+)
