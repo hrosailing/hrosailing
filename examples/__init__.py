8d161c0272b44c5051fa57aeffbf3cf0a4a7e473
VFDannenberg <valentin.dannen@googlemail.com>
2021-06-30 17:27:37 +0200

    began examples

---
+++ examples/__init__.py
@@ -0,0 +1,12 @@
+from examples.polardiagramex import (
+    print_polar_table,
+    print_point_cloud,
+    symmetrize_table,
+    plot_polar_table,
+)
+
+from examples.processingex import (
+    visualize_ball,
+    visualize_ellipsoid,
+    visualize_cuboid,
+)
