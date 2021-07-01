b343bfbe298219bb1ad482f846ba9dca8237f6b8
VFDannenberg <valentin.dannen@googlemail.com>
2021-07-01 14:27:11 +0200

    refractored

--- hrosailing/utils.py
+++ hrosailing/utils.py
@@ -12,11 +12,5 @@ from typing import Iterable
 # TODO: Error checks?
 
 
-def polar_to_kartesian(arr):
-    return np.column_stack(
-        (arr[:, 1] * np.cos(arr[:, 0]),
-         arr[:, 1] * np.sin(arr[:, 0])))
-
-
 def euclidean_norm(vec):
     return np.linalg.norm(vec, axis=1)
