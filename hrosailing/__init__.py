7108c437d2817c6d0381df5ba4ac33ff55c4f8de
VFDannenberg <valentin.dannen@googlemail.com>
2021-07-15 12:19:34 +0200

    reformatting

--- hrosailing/__init__.py
+++ hrosailing/__init__.py
@@ -1,7 +1,4 @@
 import hrosailing.polardiagram
 import hrosailing.processing
 
-from hrosailing.wind import (
-    true_wind_to_apparent,
-    apparent_wind_to_true,
-)
+from hrosailing.wind import true_wind_to_apparent, apparent_wind_to_true
