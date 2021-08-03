37df6aaa9334d61b09d0d330e076b0c91a0e3bf3
VFDannenberg <valentin.dannen@googlemail.com>
2021-08-03 11:22:36 +0200

    made code a bit cleaner

--- hrosailing/cruising/__init__.py
+++ hrosailing/cruising/__init__.py
@@ -8,27 +8,33 @@ import numpy as np
 from scipy.spatial import ConvexHull
 
 
-# TODO A bit cleaner, some error checks!
 def convex_direction(pd: pol.PolarDiagram, ws, direction):
+    """
+
+    """
     _, wa, bsp = pd.get_slices(ws)
     bsp = bsp.ravel()
-    kart = np.column_stack((bsp * np.cos(wa)), bsp * np.sin(wa))
-    vert = ConvexHull(kart).vertices
-    direction = np.deg2rad(direction)
-    conv = sorted(
-        [(i, abs(wa[i] - direction)) for i in vert], key=lambda x: x[1]
-    )
-    if not conv[0][1]:
-        i = conv[0][0]
-        return [(wa[i], 1)]
+    vert = ConvexHull(
+        np.column_stack((bsp * np.cos(wa)), bsp * np.sin(wa))
+    ).vertices
 
-    i1, i2 = conv[0][0], conv[1][0]
+    # since wind angles are in rad, we
+    # convert direction from deg to rad too.
+    # Makes it easier to calculate distances
+    direction = np.deg2rad(direction)
+    edge = sorted(
+        [(i, abs(wa[i] - direction)) for i in vert], key=lambda x: x[1]
+    )[:2]
+    if not edge[0][1]:
+        i = edge[0][0]
+        return [(np.rad2deg(wa[i]), 1)]
+    i1, i2 = edge[0][0], edge[1][0]
     lambd = (direction - wa[i2]) / (wa[i1] - wa[i2])
+
     if lambd <= 0:
         raise ValueError("something went wrong")
 
-    # Convert to degrees?
-    return [(wa[i1], lambd), (wa[i2], 1 - lambd)]
+    return [(np.rad2deg(wa[i1]), lambd), (np.rad2deg(wa[i2]), 1 - lambd)]
 
 
 def cruise(pd: pol.PolarDiagram, ws, wa, start, end):
