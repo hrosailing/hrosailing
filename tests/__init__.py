6c8461b94a5031822e527f360d958424748a05e9
VFDannenberg <valentin.dannen@googlemail.com>
2021-08-09 13:17:46 +0200

    formatted with black

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,14 +3,8 @@ from tests.polardiagramtests import (
     point_cloud_suite,
     polar_curve_suite,
 )
-from tests.windtest import (
-    speed_resolution_suite,
-    angle_resolution_suite,
-)
-from tests.windconversiontests import (
-    apparent_wind_suite,
-    true_wind_suite,
-)
+from tests.windtest import speed_resolution_suite, angle_resolution_suite
+from tests.windconversiontests import apparent_wind_suite, true_wind_suite
 from tests.plottingtests import (
     table_plot_suite,
     curve_plot_suite,
