8f92b18f5c4d5bc314c2366c69247488524da3d0
VFDannenberg <valentin.dannen@googlemail.com>
2021-08-10 16:44:03 +0200

    added read_nmea_file and read_csv_file to __init__.py

--- hrosailing/processing/__init__.py
+++ hrosailing/processing/__init__.py
@@ -1,4 +1,4 @@
 import hrosailing.processing.pipelinecomponents
 import hrosailing.processing.modelfunctions
 
-from hrosailing.processing.processing import PolarPipeline
+from hrosailing.processing.processing import PolarPipeline, read_csv_file, read_nmea_file
