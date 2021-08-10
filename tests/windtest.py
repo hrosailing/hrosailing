ae1499fccdfb4e02b88ec2339cedb4db1e3f6c37
VFDannenberg <valentin.dannen@googlemail.com>
2021-08-10 16:42:46 +0200

    refactored windconversiontests to windtest

--- tests/windtest.py
+++ tests/windtest.py
@@ -89,3 +89,25 @@ def set_resolution_suite():
     )
 
     return suite
+
+
+class ApparentWindTest(unittest.TestCase):
+    pass
+
+
+def apparent_wind_suite():
+    suite = unittest.TestSuite()
+    suite.addTests([])
+
+    return suite
+
+
+class TrueWindTest(unittest.TestCase):
+    pass
+
+
+def true_wind_suite():
+    suite = unittest.TestSuite()
+    suite.addTests([])
+
+    return suite
