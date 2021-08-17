9e417fe8f10e6a0a96007a537e2cdb42b4212507
VFDannenberg <valentin.dannen@googlemail.com>
2021-08-17 15:08:13 +0200

    formatted with black

--- hrosailing/__init__.py
+++ hrosailing/__init__.py
@@ -4,9 +4,9 @@ import hrosailing.processing
 from hrosailing.wind import true_wind_to_apparent, apparent_wind_to_true
 
 __pdoc__ = {
-    'hrosailing.polardiagram.plotting': False,
-    'hrosailing.processing.modelfunctions': False,
-    'hrosailing.wind.set_resolution': False,
-    'hrosailing.wind.convert_wind': False,
-    'hrosailing.processing.processing.PolarPipeline.__call__': True,
+    "hrosailing.polardiagram.plotting": False,
+    "hrosailing.processing.modelfunctions": False,
+    "hrosailing.wind.set_resolution": False,
+    "hrosailing.wind.convert_wind": False,
+    "hrosailing.processing.processing.PolarPipeline.__call__": True,
 }
