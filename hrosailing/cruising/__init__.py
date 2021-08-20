07295509144d0cbc7bc51ad06416b6d23dfa89f3
VFDannenberg <valentin.dannen@googlemail.com>
2021-08-20 14:51:57 +0200

    improved convex_direction

--- hrosailing/cruising/__init__.py
+++ hrosailing/cruising/__init__.py
@@ -16,51 +16,56 @@ from hrosailing.processing.pipelinecomponents.influencemodel import (
 # TODO Maybe change some function names
 
 
-# TODO Not yet functional for PolarDiagramMultiSails
 def convex_direction(
     pd: pol.PolarDiagram, ws, direction, im: InfluenceModel = None
 ):
     """"""
-    _, wa, bsp, *_ = pd.get_slices(ws)
+    _, wa, bsp, *members = pd.get_slices(ws)
     bsp = bsp.ravel()
-    vert = ConvexHull(
-        np.column_stack((bsp * np.cos(wa)), bsp * np.sin(wa))
-    ).vertices
+    if im is not None:
+        bsp = im.add_influence(bsp)
+
+    polar_pts = np.column_stack(
+        (bsp * np.cos(wa).ravel(), bsp * np.sin(wa).ravel())
+    )
+    conv = ConvexHull(polar_pts)
+    vert = conv.vertices
 
     direction = np.deg2rad(direction)
     edge = sorted(
-        [(i, abs(wa[i] - direction)) for i in vert], key=lambda x: x[1]
-    )[:2]
+        [
+            (i, abs(wa[i] - direction), members[i] if members else 0)
+            for i in vert
+        ],
+        key=lambda x: x[1],
+    )
+    edge = edge[:2]
+
     if not edge[0][1]:
         i = edge[0][0]
-        return [(np.rad2deg(wa[i]), 1)]
+        return [(np.rad2deg(wa[i]), 1, edge[0][2])]
 
     i1, i2 = edge[0][0], edge[1][0]
     # if direction lies on an edge of the polar diagram, which
     # is also an edge of the convex hull, we can sail straight
     # in direction.
-    if (
-        abs(i1 - i2) == 1
-        or (i1 == 0 and i2 == len(wa) - 1)
-        or (i1 == len(wa) - 1 and i2 == 0)
-    ):
-        # Maybe doesn't work for Pointcloud?, but should
-        # work for Table and Curve
-        return [(np.rad2deg(direction), 1)]
+    if abs(i1 - i2) == 1 and edge[0][2] == edge[1][2]:
+        return [(np.rad2deg(direction), 1, edge[0][2])]
 
-    lambd = (direction - wa[i2]) / (wa[i1] - wa[i2])
-    if lambd <= 0:
-        raise ValueError("something went wrong")
+    lambda_ = (direction - wa[i2]) / (wa[i1] - wa[i2])
 
-    return [(np.rad2deg(wa[i1]), lambd), (np.rad2deg(wa[i2]), 1 - lambd)]
+    return [
+        (np.rad2deg(wa[i1]), lambda_, edge[0][2]),
+        (np.rad2deg(wa[i2]), 1 - lambda_, edge[1][2]),
+    ]
 
 
 def cruise(
     pd: pol.PolarDiagram, ws, wa, start, end, im: InfluenceModel = None
 ):
     """"""
-    _, _, bsp, *_ = pd.get_slices(ws)
-    (wa1, lambd1), (wa2, lambd2) = convex_direction(pd, ws, wa)
+    _, _, bsp, *members = pd.get_slices(ws)
+    (wa1, lambd1), (wa2, lambd2) = convex_direction(pd, ws, wa, im)
     dist = ...
     t1, t2 = ..., ...
 
