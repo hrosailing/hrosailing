8e6028159eacdaa7373f32db8e4a7f938ce3d2c5
Ente <ente@entenhausen.de>
2021-08-26 13:33:45 +0200

    expanded __pdoc__ for documentation to now include certain dundermethods of a few classes

--- hrosailing/__init__.py
+++ hrosailing/__init__.py
@@ -4,10 +4,20 @@ import hrosailing.processing
 from hrosailing.wind import true_wind_to_apparent, apparent_wind_to_true
 
 __pdoc__ = {
-    "hrosailing.polardiagram.plotting": False,
-    "hrosailing.processing.modelfunctions": False,
     "hrosailing.wind.set_resolution": False,
     "hrosailing.wind.convert_wind": False,
+    "hrosailing.polardiagram.plotting": False,
+    "hrosailing.polardiagram.PolarDiagramTable.__getitem__": True,
+    "hrosailing.polardiagram.PolarDiagramTable.__str__": True,
+    "hrosailing.polardiagram.PolarDiagramTable.__repr__": True,
+    "hrosailing.polardiagram.PolarDiagramMultiSails.__getitem__": True,
+    "hrosailing.polardiagram.PolarDiagramMultiSails.__str__": True,
+    "hrosailing.polardiagram.PolarDiagramMultiSails.__repr__": True,
+    "hrosailing.polardiagram.PolarDiagramCurve.__repr__": True,
+    "hrosailing.polardiagram.PolarDiagramCurve.__call__": True,
+    "hrosailing.polardiagram.PolarDiagramPointcloud.__str__": True,
+    "hrosailing.polardiagram.PolarDiagramPointcloud.__repr__": True,
+    "hrosailing.processing.modelfunctions": False,
     "hrosailing.processing.pipeline.PolarPipeline.__call__": True,
     "hrosailing.processing.pipelinecomponents.interpolator.gauss_potential": False,
     "hrosailing.processing.pipelinecomponents.interpolator.scaled": False,
@@ -17,5 +27,7 @@ __pdoc__ = {
     "hrosailing.processing.pipelinecomponents.sampler.make_circle": False,
     "hrosailing.processing.pipelinecomponents.weigher.euclidean_norm": False,
     "hrosailing.processing.pipelinecomponents.weigher.scaled": False,
-
+    "hrosailing.processing.pipelinecomponents.weigher.WeightedPoints.__getitem__": True,
+    "hrosailing.processing.pipelinecomponents.weigher.CylindricMeanWeigher.__repr__": True,
+    "hrosailing.processing.pipelinecomponents.weigher.CylindricMemberWeigher.__repr__": True,
 }
