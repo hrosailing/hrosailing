ace1041dabf1049426f0959746a752a6a8409604
Ente <ente@entenhausen.de>
2021-09-08 10:44:07 +0200

    finished first implementation for ShepardInterpolator

--- hrosailing/processing/pipelinecomponents/interpolator.py
+++ hrosailing/processing/pipelinecomponents/interpolator.py
@@ -14,6 +14,8 @@ from typing import Callable
 
 import numpy as np
 
+from .neighbourhood import Neighbourhood
+
 
 def scaled(norm, scal):
     scal = np.array(list(scal))
@@ -231,8 +233,6 @@ class ImprovedIDWInterpolator(Interpolator):
     on the work of Shepard, "A two-dimensional interpolation
     function for irregulary-spaced data"
 
-    Should (only) be used together with the ScalingBall neighbourhood
-
     Instead of setting the weights as the normal inverse distance
     to some power, we set the weights in the following way:
 
@@ -298,13 +298,10 @@ class ImprovedIDWInterpolator(Interpolator):
         return wts @ pts[:, 2]
 
 
-# TODO Finish implementation
 class ShepardInterpolator(Interpolator):
     """A full featured inverse distance interpolator, based
     on the work of Shepard, "A two-dimensional interpolation
     function for irregulary-spaced data"
-
-    Should (only) be used together with the ScalingBall neighbourhood
 
 
     Parameters
@@ -330,6 +327,7 @@ class ShepardInterpolator(Interpolator):
 
     def __init__(
         self,
+        neighbourhood: Neighbourhood,
         tol=np.finfo(float).eps,
         slope=0.1,
         norm: Callable = scaled(euclidean_norm, (1 / 40, 1 / 360)),
@@ -342,6 +340,7 @@ class ShepardInterpolator(Interpolator):
         self._norm = norm
         self._tol = tol
         self._slope = slope
+        self._neighbourhood = neighbourhood
 
     def __repr__(self):
         return (
@@ -374,19 +373,30 @@ class ShepardInterpolator(Interpolator):
 
         wts = _set_weights(pts, dist)
         wts = _include_direction(pts, grid_pt, dist, wts)
-        wts /= np.sum(wts)
+        zdelta = _determine_slope(
+            pts,
+            grid_pt,
+            dist,
+            wts,
+            self._neighbourhood,
+            self._norm,
+            self._slope,
+        )
 
+        # reduce computational errors
         mask = dist < self._tol
         n_eps = pts[mask]
         if n_eps.size:
             return np.sum(n_eps[:, 2]) / n_eps.shape[0]
 
-        return wts @ (pts[:, 2])
+        wts /= np.sum(wts)
+        return wts @ (pts[:, 2] + zdelta)
 
 
 def _set_weights(pts, dist):
     wts = np.zeros(pts.shape[0])
     r = np.max(dist)
+
     for i, d in enumerate(dist):
         if 0 < d <= r / 3:
             wts[i] = 1 / d
@@ -398,18 +408,51 @@ def _set_weights(pts, dist):
 
 def _include_direction(pts, grid_pt, dist, wts):
     t = np.zeros(pts.shape[0])
+
     for i, pt in enumerate(pts):
-        t[i] = _sum_cosines(wts, pt, i, pts, grid_pt, dist)
+        cosine = (grid_pt[0] - pt[0]) * (
+            grid_pt[0] - np.delete(pts, i, axis=0)[:, 0]
+        ) + (grid_pt[1] - pt[1]) * (
+            grid_pt[1] - np.delete(pts, i, axis=0)[:, 0]
+        )
+        cosine /= dist[i] * np.delete(dist, i)
+        t[i] = np.sum(np.delete(wts, i) * (1 - cosine))
         t[i] /= np.sum(wts)
 
     return np.square(wts) * (1 + t)
 
 
-def _sum_cosines(wts, pt, i, pts, grid_pt, distances):
+def _determine_slope(pts, grid_pt, dist, wts, nhood, norm, slope):
+    a = np.zeros(pts.shape[0])
+    b = np.zeros(pts.shape[0])
 
-    cosine = (grid_pt[0] - pt[0]) * (
-        grid_pt[0] - np.delete(pts, i, axis=0)[:, 0]
-    ) + (grid_pt[1] - pt[1]) * (grid_pt[1] - np.delete(pts, i, axis=0)[:, 0])
-    cosine /= distances[i] * np.delete(distances, i)
+    for i, pt in enumerate(pts):
+        ptsi = np.delete(pts, i, axis=0)
+        mask = nhood.is_contained_in(ptsi - pt)
+        ptsi = ptsi[mask]
+        disti = norm(ptsi - pt)
+        wtsi = np.delete(wts, i)[mask]
+        ai = (
+            wtsi
+            * (ptsi[:, 2] - pt[:, 2])
+            * (ptsi[:, 0] - pt[:, 0])
+            / np.square(disti)
+        )
+        a[i] = np.sum(ai) / np.sum(wtsi)
+        bi = (
+            wtsi
+            * (ptsi[:, 2] - pt[:, 2])
+            * (ptsi[:, 1] - pt[:, 1])
+            / np.square(disti)
+        )
+        b[i] = np.sum(bi) / np.sum(wtsi)
 
-    return np.sum(np.delete(wts, i) * (1 - cosine))
+    v = (
+        slope
+        * (np.max(pts[:, 2]) - np.min(pts[:, 2]))
+        / np.sqrt(np.max(np.square(a) + np.square(b)))
+    )
+
+    return (a * (grid_pt[0] - pts[:, 0]) + b * (grid_pt[1] - pts[:, 1])) * (
+        v / (v + dist)
+    )
