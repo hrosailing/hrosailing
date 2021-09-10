8083f59885fb274b5bb087e355ff98c37d925e2a
Ente <ente@entenhausen.de>
2021-09-10 14:32:43 +0200

    "antisymmetrized" modelfunctions

--- hrosailing/processing/modelfunctions/__init__.py
+++ hrosailing/processing/modelfunctions/__init__.py
@@ -9,54 +9,69 @@ from ._models2d import *
 
 
 def ws_times_wa(ws, wa, scal):
-    ws = np.asarray(ws)
-    wa = np.asarray(wa)
     return scal * ws * wa
 
 
 def ws_concave_dt_wa(ws, wa, *params):
-    ws = np.asarray(ws)
     wa = np.asarray(wa)
+
     return (
         concave_function(ws, params[0], params[1], params[2])
         + inverted_shifted_parabola(wa, params[3], params[4], params[5])
         + ws_times_wa(ws, wa, params[6])
+        + inverted_shifted_parabola(360 - wa, params[7], params[8], params[9])
+        + ws_times_wa(ws, 360 - wa, params[10])
     )
 
 
 def ws_wa_s_dt(ws, wa, *params):
-    ws = np.asarray(ws)
     wa = np.asarray(wa)
+
     return (
         s_shaped(ws, params[0], params[1], params[2], params[3], downturn=True)
         + s_shaped(
             wa, params[4], params[5], params[6], params[7], downturn=True
         )
         + ws_times_wa(ws, wa, params[8])
+        + s_shaped(
+            360 - wa,
+            params[9],
+            params[10],
+            params[11],
+            params[12],
+            downturn=True,
+        )
+        + ws_times_wa(ws, 360 - wa, params[13])
     )
 
 
 def ws_s_dt_wa_gauss(ws, wa, *params):
-    ws = np.asarray(ws)
     wa = np.asarray(wa)
-    return s_shaped(
-        ws, params[0], params[1], params[2], params[3], downturn=True
-    ) + gaussian_model(wa, params[4], params[5], params[6])
+
+    return (
+        s_shaped(ws, params[0], params[1], params[2], params[3], downturn=True)
+        + gaussian_model(wa, params[4], params[5], params[6])
+        + gaussian_model(360 - wa, params[7], params[8], params[9])
+    )
 
 
 def ws_s_s_dt_wa_gauss_comb(ws, wa, *params):
-    ws = np.asarray(ws)
     wa = np.asarray(wa)
+
     return (
         s_shaped(ws, params[0], params[1], params[2], params[3], downturn=True)
         + gaussian_model(wa, params[4], params[5], params[6])
         + ws_times_wa(ws, wa, params[7])
+        + gaussian_model(360 - wa, params[8], params[9], params[10])
+        + ws_times_wa(ws, 360 - wa, params[11])
     )
 
 
 def ws_s_wa_gauss(ws, wa, *params):
-    ws = np.asarray(ws)
     wa = np.asarray(wa)
-    return s_shaped(
-        ws, params[0], params[1], params[2], params[3]
-    ) + gaussian_model(wa, params[4], params[5], params[6])
+
+    return (
+        s_shaped(ws, params[0], params[1], params[2], params[3])
+        + gaussian_model(wa, params[4], params[5], params[6])
+        + gaussian_model(360 - wa, params[7], params[8], params[9])
+    )
--- hrosailing/processing/modelfunctions/_models2d.py
+++ hrosailing/processing/modelfunctions/_models2d.py
@@ -8,60 +8,63 @@
 import numpy as np
 
 
-def polynomial_function(x, *args, deg=1):
+def polynomial_function(x, *params, deg=1):
     val = 0
     for i in range(deg + 1):
-        val += args[i] * np.power(x, i)
+        val += params[i] * np.power(x, i)
 
     return val
 
 
-def inverted_parabola(x, *args):
-    return args[1] + args[2] * np.square(x - args[0])
+def inverted_parabola(x, *params):
+    return params[1] + params[2] * np.square(x - params[0])
 
 
-def inverted_shifted_parabola(x, *args):
-    return args[0] * x + args[2] * np.square(x - args[1])
+def inverted_shifted_parabola(x, *params):
+    return params[0] * x + params[2] * np.square(x - params[1])
 
 
-def concave_function(x, *args, downturn=True, sat_limit=False):
+def concave_function(x, *params, downturn=True, sat_limit=False):
     if sat_limit and downturn:
-        return args[2] - args[0] * np.exp(args[1] - x) - args[3] * np.square(x)
-
-    if sat_limit:
-        return args[2] - args[0] * np.exp(args[1] - x)
-
-    if downturn:
-        return polynomial_function(x, args[0], args[1], -args[2], deg=2)
-
-
-def s_shaped(x, *args, downturn=False):
-    val = args[2] / (1 + np.exp(args[0] - args[1] * x))
-    if downturn:
-        val -= args[3] * np.square(x)
-
-    return val
-
-
-def gompertz_model(x, *args, neg=False):
-    if neg:
-        return args[2] * np.exp(-args[0] * np.power(-args[1], x))
-
-    return args[2] * np.exp(-args[0] * np.power(args[1], x))
-
-
-def gaussian_model(x, *args, offset=False):
-    if offset:
         return (
-            args[0] * np.exp(-0.5 * np.square(x - args[1]) / args[2]) + args[3]
+            params[2]
+            - params[0] * np.exp(params[1] - x)
+            - params[3] * np.square(x)
         )
 
-    return args[0] * np.exp(-0.5 * np.square(x - args[1]) / args[2])
+    if sat_limit:
+        return params[2] - params[0] * np.exp(params[1] - x)
+
+    if downturn:
+        return polynomial_function(x, params[0], params[1], -params[2], deg=2)
 
 
-def gmm_model(x, *args):
+def s_shaped(x, *params, downturn=False):
+    val = params[2] / (1 + np.exp(params[0] - params[1] * x))
+    if downturn:
+        val -= params[3] * np.square(x)
+
+    return val
+
+
+def gompertz_model(x, *params, neg=False):
+    if neg:
+        return params[2] * np.exp(-params[0] * np.power(-params[1], x))
+
+    return params[2] * np.exp(-params[0] * np.power(params[1], x))
+
+
+def gaussian_model(x, *params, offset=False):
+    val = params[0] * np.exp(-0.5 * np.square(x - params[1]) / params[2])
+    if offset:
+        val += params[3]
+
+    return val
+
+
+def gmm_model(x, *params):
     return (
-        args[0] * np.exp(-0.5 * np.square(x - args[1]) / args[2])
-        + args[3] * np.exp(-0.5 * np.square(x - args[4]) / args[5])
-        + args[6]
+        params[0] * np.exp(-0.5 * np.square(x - params[1]) / params[2])
+        + params[3] * np.exp(-0.5 * np.square(x - params[4]) / params[5])
+        + params[6]
     )
