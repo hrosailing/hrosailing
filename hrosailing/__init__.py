d57dec1f3ef6bfeab3390a4a588ed097403b14a0
Ente <ente@entenhausen.de>
2021-09-15 14:57:40 +0200

    refractored module docstring

--- hrosailing/__init__.py
+++ hrosailing/__init__.py
@@ -1,29 +1,6 @@
-"""
-The Python package hrosailing provides classes and functions ....
-polar diagrams .... sailing .... from data ... pipeline ... machine learning
-... modular ....
+from ._doc import doc
 
-Installation
-------------
-hrosailing can be installed using `pip install hrosailing`. It requires
-Python ..., aswell as numpy ..., matplotlib ... and scipy ... to run.
-
-Getting Started
----------------
-
-Contributing
-------------
-
-License
--------
-
-Authors
--------
-Valentin Dannenberg (valentin.dannenberg2@uni-rostock.de)
-
-Robert Schueler (robert.schueler2@uni-rostock.de)
-"""
-
+__doc__ = doc
 
 # Tell users if hard depencencies are missing
 hard_dependencies = ("numpy", "matplotlib", "scipy")
---
+++ hrosailing/_doc.py
@@ -0,0 +1,24 @@
+doc = """The Python package hrosailing provides classes and functions ....
+polar diagrams .... sailing .... from data ... pipeline ... machine learning
+... modular ....
+
+Installation
+------------
+hrosailing can be installed using `pip install hrosailing`. It requires
+Python ..., aswell as numpy ..., matplotlib ... and scipy ... to run.
+
+Getting Started
+---------------
+
+Contributing
+------------
+
+License
+-------
+
+Authors
+-------
+Valentin Dannenberg (valentin.dannenberg2@uni-rostock.de)
+
+Robert Schueler (robert.schueler2@uni-rostock.de)
+"""
