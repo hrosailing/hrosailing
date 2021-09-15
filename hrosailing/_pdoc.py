e8ecca5965374aa05d9fb4a5d3b990c5d567dd78
Ente <ente@entenhausen.de>
2021-09-15 11:42:04 +0200

    moved __pdoc__ dic to own file and removed it from __init__.py

---
+++ hrosailing/_pdoc.py
@@ -0,0 +1,16 @@
+pdoc = {
+    "hrosailing.wind.set_resolution": False,
+    "hrosailing.wind.convert_wind": False,
+    "hrosailing.polardiagram.PolarDiagramTable.__getitem__": True,
+    "hrosailing.polardiagram.PolarDiagramMultiSails.__getitem__": True,
+    "hrosailing.polardiagram.PolarDiagramCurve.__call__": True,
+    "hrosailing.pipeline.pipeline.PolarPipeline.__call__": True,
+    "hrosailing.pipeline.pipelinecomponents.interpolator.gauss_potential": False,
+    "hrosailing.pipeline.pipelinecomponents.interpolator.scaled": False,
+    "hrosailing.pipeline.pipelinecomponents.interpolator.euclidean_norm": False,
+    "hrosailing.pipeline.pipelinecomponents.neighbourhood.euclidean_norm": False,
+    "hrosailing.pipeline.pipelinecomponents.neighbourhood.scaled": False,
+    "hrosailing.pipeline.pipelinecomponents.sampler.make_circle": False,
+    "hrosailing.pipeline.pipelinecomponents.weigher.euclidean_norm": False,
+    "hrosailing.pipeline.pipelinecomponents.weigher.scaled": False,
+}
