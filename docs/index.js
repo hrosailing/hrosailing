0f3da32107bd5f551bf360c8a629e8cbb0d67e0e
Ente <ente@entenhausen.de>
2021-09-15 11:42:55 +0200

    updated docs (not yet live on gh pages )

--- docs/index.js
+++ docs/index.js
@@ -19,7 +19,7 @@ INDEX=[
 {
 "ref":"hrosailing",
 "url":0,
-"doc":"The Python package hrosailing provides classes and functions   polar diagrams   sailing   from data  . pipeline  . machine learning  . modular   Installation       hrosailing can be installed using  pip install hrosailing . It requires Python  . to run. Getting Started        - References      Compatability       - Contributing       License    - Authors    - Valentin Dannenberg (valentin.dannenberg2@uni-rostock.de) Robert Schueler (robert.schueler2@uni-rostock.de)"
+"doc":"The Python package hrosailing provides classes and functions   polar diagrams   sailing   from data  . pipeline  . machine learning  . modular   Installation       hrosailing can be installed using  pip install hrosailing . It requires Python  ., aswell as numpy  ., matplotlib  . and scipy  . to run. Getting Started        - Contributing       License    - Authors    - Valentin Dannenberg (valentin.dannenberg2@uni-rostock.de) Robert Schueler (robert.schueler2@uni-rostock.de)"
 },
 {
 "ref":"hrosailing.polardiagram",
