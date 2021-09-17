b1805667ddaf61fc201f1b4c377b2b08be437c15
VFDannenberg <valentin.dannen@googlemail.com>
2021-09-17 12:51:15 +0200

    renamed files to work with pytest

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,17 +1,17 @@
-from tests.polardiagramtests import (
+from tests.polardiagram_test import (
     polar_table_suite,
     point_cloud_suite,
     polar_curve_suite,
 )
-from tests.windtest import (
+from tests.wind_test import (
     set_resolution_suite,
     apparent_wind_suite,
     true_wind_suite,
 )
-from tests.plottingtests import (
+from tests.plotting_test import (
     table_plot_suite,
     curve_plot_suite,
     pointcloud_plot_suite,
 )
 
-from tests.writingreadingtests import reading_suite, writing_suite
+from tests.writingreading_test import reading_suite, writing_suite
