94464fa84c16443e98386f9deb1d921d91c6bae9
Ente <ente@entenhausen.de>
2021-09-20 14:26:25 +0200

    shortened imports a bit

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,17 +1,20 @@
-from tests.polardiagram_test import (
+from .polardiagram_test import (
     polar_table_suite,
     point_cloud_suite,
     polar_curve_suite,
 )
-from tests.wind_test import (
+from .wind_test import (
     set_resolution_suite,
     apparent_wind_suite,
     true_wind_suite,
 )
-from tests.plotting_test import (
+from .plotting_test import (
     table_plot_suite,
     curve_plot_suite,
     pointcloud_plot_suite,
 )
 
-from tests.writingreading_test import reading_suite, writing_suite
+from .writingreading_test import (
+    reading_suite,
+    writing_suite
+)
