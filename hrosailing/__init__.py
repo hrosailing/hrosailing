f1ab2d6c0ae0df2f87bb05db5872809b7268f149
Ente <ente@entenhausen.de>
2021-09-28 09:54:35 +0200

    cleaned up init a bit

--- hrosailing/__init__.py
+++ hrosailing/__init__.py
@@ -1,3 +1,22 @@
+from ._doc import doc
+from ._version import __version__
+from ._pdoc import pdoc
+
+__doc__ = doc
+version = __version__
+__pdoc__ = pdoc
+
+
+# create folder for logging, where script is located, if it doesn't exist
+import hrosailing._logfolder as log
+
+path = log.get_script_path()
+log.set_log_folder(path)
+log.create_log_folder()
+
+del log
+
+
 # Tell users if hard depencencies are missing
 hard_dependencies = ("numpy", "matplotlib", "scipy")
 # soft_dependencies = ("pandas", "pynmea2")
@@ -17,30 +36,8 @@ if missing_dependencies:
 del hard_dependencies, depencency, missing_dependencies
 
 
-# create folder for logging, where script is located, if it doesn't exist
-import hrosailing._logfolder as log
-
-path = log.get_script_path()
-log.set_log_folder(path)
-log.create_log_folder()
-
-del log
-
-
 import hrosailing.cruising
-import hrosailing.pipeline
 import hrosailing.pipelinecomponents
+import hrosailing.pipeline
 import hrosailing.polardiagram
 import hrosailing.wind
-
-from ._doc import doc
-
-__doc__ = doc
-
-from ._version import __version__
-
-version = __version__
-
-from ._pdoc import pdoc
-
-__pdoc__ = pdoc
