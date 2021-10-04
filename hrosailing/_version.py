3505fd4c4bdec5a24b43d20c4f26e6d9af3f8c27
Ente <ente@entenhausen.de>
2021-10-04 10:36:40 +0200

    updated setups for eventual pip wheel release

--- hrosailing/_version.py
+++ hrosailing/_version.py
@@ -1,2 +1,2 @@
 # pylint: disable=missing-docstring
-__version__ = "0.0.1"
+__version__ = "0.9.0"
--- setup.py
+++ setup.py
@@ -6,15 +6,15 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="hrosailing",
-    version="0.0.1",
-    author="Valentin Dannenberg / Robert Schueler",
+    version="0.9.0",
+    author="Valentin Dannenberg & Robert Schueler",
     author_email="valentin.dannenberg2@uni-rostock.de",
     description="Python library for Polar (Performance) Diagrams",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/VFDannenberg/hrosailing",
+    url="https://github.com/hrosailing/hrosailing",
     project_urls={
-        "Bug Tracker": "https://github.com/VFDannenberg/hrosailing/issues"
+        "Bug Tracker": "https://github.com/hrosailing/hrosailing/issues"
     },
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -22,10 +22,10 @@ setuptools.setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: Implementation :: PyPy"
-        "License :: ",
+        "License :: OSI Approved :: APACHE License",
         "Operating System :: OS Independent",
     ],
     package_dir={"": "hrosailing"},
     packages=setuptools.find_packages(where="hrosailing"),
-    python_requires=">=3.8",
+    python_requires=">=3.7",
 )
