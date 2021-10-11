708996b3300d6124fed5aa25e3df44936354b320
Ente <ente@entenhausen.de>
2021-10-11 10:19:56 +0200

    removed some unnecessary entries

--- hrosailing/_pdoc.py
+++ hrosailing/_pdoc.py
@@ -7,10 +7,4 @@ pdoc = {
     "hrosailing.polardiagram.PolarDiagramCurve.__call__": True,
     "hrosailing.pipeline.pipeline.PolarPipeline.__call__": True,
     "hrosailing.pipeline.pipelinecomponents.interpolator.gauss_potential": False,
-    "hrosailing.pipeline.pipelinecomponents.interpolator.scaled": False,
-    "hrosailing.pipeline.pipelinecomponents.interpolator.euclidean_norm": False,
-    "hrosailing.pipeline.pipelinecomponents.neighbourhood.euclidean_norm": False,
-    "hrosailing.pipeline.pipelinecomponents.sampler.make_circle": False,
-    "hrosailing.pipeline.pipelinecomponents.weigher.euclidean_norm": False,
-    "hrosailing.pipeline.pipelinecomponents.weigher.scaled": False,
 }
