9e106169b0f775fce2d5ff779da01d3acb218402
Ente <ente@entenhausen.de>
2021-10-19 13:27:46 +0200

    updated __pdoc__ dict with new file paths

--- hrosailing/_pdoc.py
+++ hrosailing/_pdoc.py
@@ -3,8 +3,10 @@
 
 pdoc = {
     "hrosailing.polardiagram.PolarDiagramTable.__getitem__": True,
+    "hrosailing.polardiagram.PolarDiagramTable.__call__": True,
     "hrosailing.polardiagram.PolarDiagramMultiSails.__getitem__": True,
     "hrosailing.polardiagram.PolarDiagramCurve.__call__": True,
-    "hrosailing.pipeline.pipeline.PolarPipeline.__call__": True,
-    "hrosailing.pipeline.pipelinecomponents.interpolator.gauss_potential": False,
+    "hrosailing.polardiagram.PolarDiagramPointcloud.__call__": True,
+    "hrosailing.pipeline.PolarPipeline.__call__": True,
+    "hrosailing.pipelinecomponents.interpolator.gauss_potential": False,
 }
