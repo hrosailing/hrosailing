9267f92bd579b9a6db1760439956dff5d56af4a6
Teodor Rata <valentin.dannenberg2@uni-rostock.de>
2022-01-10 18:32:16 +0100

    changed import after refractoring

--- tests/polardiagram_test.py
+++ tests/polardiagram_test.py
@@ -7,7 +7,7 @@ import unittest
 import numpy as np
 
 import hrosailing.polardiagram as pol
-from hrosailing.polardiagram import (
+from hrosailing.polardiagram._basepolardiagram import (
     PolarDiagramException,
     PolarDiagramInitializationException,
 )
