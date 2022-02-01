8db2996b6198d563015dd8ffe63b739f86e0d9cb
autoisort <autoisort@users.noreply.github.com>
2022-02-01 10:27:27 +0000

    fixup: format imports with isort

--- tests/polardiagram_test.py
+++ tests/polardiagram_test.py
@@ -8,9 +8,7 @@ import numpy as np
 
 import hrosailing.polardiagram as pol
 from hrosailing.polardiagram._basepolardiagram import (
-    PolarDiagramException,
-    PolarDiagramInitializationException,
-)
+    PolarDiagramException, PolarDiagramInitializationException)
 from hrosailing.wind import WindConversionException
 
 
