6b9c5b5612ed274a714610d0bd8f405c06e6e37c
Robert Schueler <robert.schueler1989@gmail.com>
2022-04-12 07:46:57 +0200

    refactoring of imputator and more statistics

--- hrosailing/pipelinecomponents/imputator.py
+++ hrosailing/pipelinecomponents/imputator.py
@@ -35,20 +35,24 @@ class FillLocalImputator(Imputator):
         fill_after=lambda name, left, mu: left,
         max_time_diff=timedelta(minutes=2)
     ):
-        self._fill_before = fill_before
+        self._fill_before = \
+            lambda name, left, right, mu: fill_before(name, right, mu)
         self._fill_between = fill_between
-        self._fill_after = fill_after
+        self._fill_after = \
+            lambda name, left, right, mu: fill_after(name, left, mu)
         self._max_time_diff = max_time_diff
+        self._n_filled = 0
 
     def imputate(
         self,
         data_dict
     ):
-        n_removed_fields = len(data_dict)
+        self._n_filled = 0
+        n_removed_cols = len(data_dict)
         # remove all None fields
         data_dict = {key: value for key, value in data_dict.items()
                      if not all([v is None for v in value])}
-        n_removed_fields -= len(data_dict)
+        n_removed_cols -= len(data_dict)
 
         last_dt, last_i = None, None
         for i, dt in enumerate(data_dict["datetime"]):
@@ -57,8 +61,7 @@ class FillLocalImputator(Imputator):
             elif last_dt is None:
                 pass
             elif abs(dt - last_dt) > self._max_time_diff:
-                # remove all rows where datetime points are too far apart in time
-                remove_rows.extend(list(range(last_i+1, i)))
+                continue
             else:
                 # linear approximation of time in between
                 for j in range(last_i+1, i):
@@ -94,12 +97,13 @@ class FillLocalImputator(Imputator):
                     [i for i in range(idx[0])
                      if datetime[idx[0]] - datetime[i] < self._max_time_diff]
                 ) #first idx in time interval
-                self._fill_range_before(
+                self._fill_range(
                     data_dict,
                     datetime,
                     key,
                     start_idx,
-                    idx[0]
+                    idx[0],
+                    self._fill_before
                 )
 
             # convex interpolation of entries between non-None entries
@@ -107,12 +111,13 @@ class FillLocalImputator(Imputator):
                 timediff = datetime[idx2] - datetime[idx1]
                 if timediff < self._max_time_diff:
                     #fill data according to fill_between function
-                    self._fill_range_between(
+                    self._fill_range(
                         data_dict,
                         datetime,
                         key,
                         idx1,
-                        idx2
+                        idx2,
+                        self._fill_between
                     )
                 else:
                     #fill data according to fill_before and fill_after
@@ -121,24 +126,26 @@ class FillLocalImputator(Imputator):
                          if datetime[i] - datetime[idx1] < self._max_time_diff]
                     if len(near_points) > 0:
                         last_idx_right = max(near_points)
-                        self._fill_range_after(
+                        self._fill_range(
                             data_dict,
                             datetime,
                             key,
                             idx1,
-                            last_idx_right
+                            last_idx_right,
+                            self._fill_after
                         )
                     near_points = \
                         [i for i in range(idx1 + 1, idx2)
                          if datetime[idx2] - datetime[i] < self._max_time_diff]
                     if len(near_points) > 0:
                         first_idx_left = min(near_points)
-                        self._fill_range_before(
+                        self._fill_range(
                             data_dict,
                             datetime,
                             key,
                             first_idx_left,
-                            idx2
+                            idx2,
+                            self._fill_before
                         )
 
             #fill last entries according to 'fill_after'
@@ -147,12 +154,13 @@ class FillLocalImputator(Imputator):
                  if datetime[idx[0]] - datetime[i] < self._max_time_diff]
             if len(near_points) > 0:
                 end_idx = min(near_points) #first idx in time interval
-                self._fill_range_after(
+                self._fill_range(
                     data_dict,
                     datetime,
                     key,
                     start_idx,
-                    end_idx
+                    end_idx,
+                    self._fill_after
             )
 
         #remove rows which still have None values
@@ -166,19 +174,23 @@ class FillLocalImputator(Imputator):
         n_removed_rows += len(remove_rows)
 
         statistics = {
-            "n_removed_fields": n_removed_fields,
+            "n_removed_cols": n_removed_cols,
             "n_removed_rows": n_removed_rows,
+            "n_filled_fields": self._n_filled,
+            "n_rows": len(list(data_dict.values())[0]),
+            "n_cols": len(data_dict)
         }
 
         return data_dict, statistics
 
-    def _fill_range_between(
+    def _fill_range(
             self,
             data_dict,
             datetime,
             key,
             start_idx,
-            end_idx
+            end_idx,
+            fill_fun
     ):
         left = data_dict[key][start_idx]
         right = data_dict[key][end_idx]
@@ -188,38 +200,5 @@ class FillLocalImputator(Imputator):
                 mu = (datetime[i] - datetime[end_idx])/duration
             except ZeroDivisionError:
                 mu = 0
-            data_dict[key][i] = self._fill_between(key, left, right, mu)
-
-    def _fill_range_before(
-            self,
-            data_dict,
-            datetime,
-            key,
-            start_idx,
-            end_idx
-    ):
-        right = data_dict[key][end_idx]
-        for i in range(start_idx + 1, end_idx):
-            duration = (datetime[end_idx] - datetime[start_idx])
-            try:
-                mu = (datetime[i] - datetime[end_idx]) / duration
-            except ZeroDivisionError:
-                mu = 0
-            data_dict[key][i] = self._fill_before(key, right, mu)
-
-    def _fill_range_after(
-            self,
-            data_dict,
-            datetime,
-            key,
-            start_idx,
-            end_idx
-    ):
-        left = data_dict[key][start_idx]
-        for i in range(start_idx + 1, end_idx):
-            duration = (datetime[end_idx] - datetime[start_idx])
-            try:
-                mu = (datetime[i] - datetime[end_idx]) / duration
-            except ZeroDivisionError:
-                mu = 0
-            data_dict[key][i] = self._fill_after(key, left, mu)
+            data_dict[key][i] = fill_fun(key, left, right, mu)
+            self._n_filled += 1
