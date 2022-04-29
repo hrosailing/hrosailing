090b0a21cb9eaf20f75afbc80bd2209b5884de26
autoblack <autoblack@users.noreply.github.com>
2022-04-29 21:14:26 +0000

    fixup: format code with black

--- hrosailing/_pdoc.py
+++ hrosailing/_pdoc.py
@@ -3,5 +3,5 @@
 pdoc = {
     "hrosailing.pipeline.PolarPipeline.__call__": True,
     "hrosailing.polardiagram._basepolardiagram.PolarDiagramException": True,
-    "hrosailing.polardiagram._basepolardiagram.PolarDiagramInitializationException": True
+    "hrosailing.polardiagram._basepolardiagram.PolarDiagramInitializationException": True,
 }
