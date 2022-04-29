a0f690732c449732b2172385dd7f4c4a0803a23e
VFDannenberg <valentin.dannen@googlemail.com>
2022-04-29 12:54:17 +0200

    added `PolarDiagram(Initialization)Exception to documentation`

--- hrosailing/_pdoc.py
+++ hrosailing/_pdoc.py
@@ -2,4 +2,6 @@
 
 pdoc = {
     "hrosailing.pipeline.PolarPipeline.__call__": True,
+    "hrosailing.polardiagram._basepolardiagram.PolarDiagramException": True,
+    "hrosailing.polardiagram._basepolardiagram.PolarDiagramInitializationException": True
 }
