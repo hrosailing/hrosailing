f58dbc2bd79a01a8953a031c429a9e3196e00576
Robert Schueler <robert.schueler1989@gmail.com>
2022-05-30 17:07:27 +0200

    Added globe_model.py with description

---
+++ hrosailing/globe_model/__init__.py
@@ -0,0 +1,5 @@
+"""
+Contains a base class and several concrete implementations of globe models
+which can be used to perform calculations on a three dimensional model of the
+globe using lattitude/longitude coordinates.
+"""
