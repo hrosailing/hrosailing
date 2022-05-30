80eecca31b051220def06f08f7a9965b42b1b849
Robert Schueler <robert.schueler1989@gmail.com>
2022-05-30 17:11:52 +0200

    Added base class GlobeModel

--- hrosailing/globe_model/__init__.py
+++ hrosailing/globe_model/__init__.py
@@ -3,3 +3,22 @@ Contains a base class and several concrete implementations of globe models
 which can be used to perform calculations on a three dimensional model of the
 globe using lattitude/longitude coordinates.
 """
+
+from abc import ABC, abstractmethod
+
+
+class GlobeModel(ABC):
+    """
+    Abstract base class of globe models.
+
+    Abstract Methods
+    ----------------
+    project(points)
+
+    lat_lon(point)
+
+    distance(start, end)
+
+    shortest_path(start, end, res)
+    """
+    pass
