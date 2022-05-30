942f3f37e8d526d4b8a978ee01d4c93270e49f3b
autoisort <autoisort@users.noreply.github.com>
2022-05-30 14:22:02 +0000

    fixup: format imports with isort

--- hrosailing/pipelinecomponents/__init__.py
+++ hrosailing/pipelinecomponents/__init__.py
@@ -3,16 +3,21 @@ Components for the PolarPipeline and PipelineExtension
 classes among other things
 """
 
-from .datahandler import (ArrayHandler, CsvFileHandler, DataHandler,
-                          NMEAFileHandler)
+from .datahandler import ArrayHandler, CsvFileHandler, DataHandler, NMEAFileHandler
 from .filter import BoundFilter, Filter, QuantileFilter
 from .influencemodel import InfluenceModel
-from .interpolator import (ArithmeticMeanInterpolator, IDWInterpolator,
-                           ImprovedIDWInterpolator, Interpolator)
-from .neighbourhood import (Ball, Cuboid, Ellipsoid, Neighbourhood, Polytope,
-                            ScalingBall)
+from .interpolator import (
+    ArithmeticMeanInterpolator,
+    IDWInterpolator,
+    ImprovedIDWInterpolator,
+    Interpolator,
+)
+from .neighbourhood import Ball, Cuboid, Ellipsoid, Neighbourhood, Polytope, ScalingBall
 from .regressor import LeastSquareRegressor, ODRegressor, Regressor
-from .sampler import (ArchimedianSampler, FibonacciSampler, Sampler,
-                      UniformRandomSampler)
-from .weigher import (CylindricMeanWeigher, CylindricMemberWeigher, Weigher,
-                      WeightedPoints)
+from .sampler import ArchimedianSampler, FibonacciSampler, Sampler, UniformRandomSampler
+from .weigher import (
+    CylindricMeanWeigher,
+    CylindricMemberWeigher,
+    Weigher,
+    WeightedPoints,
+)
