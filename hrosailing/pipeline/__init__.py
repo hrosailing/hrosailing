4c5f862264b9b940d7a3a689238cb06f33e94a1a
Robert Schueler <robert.schueler1989@gmail.com>
2022-06-07 14:40:51 +0200

    fixed #93

--- hrosailing/pipeline/__init__.py
+++ hrosailing/pipeline/__init__.py
@@ -238,8 +238,7 @@ class TableExtension(PipelineExtension):
         return pol.PolarDiagramTable(ws_resolution, wa_resolution, bsps)
 
     def _determine_table_size(self, points):
-        from hrosailing.polardiagram._polardiagramtable import _set_resolution
-
+        from hrosailing.polardiagram._polardiagramtable import _Resolution
         if self.wind_resolution == "auto":
             return _automatically_determined_resolution(points)
 
@@ -247,8 +246,9 @@ class TableExtension(PipelineExtension):
             self.wind_resolution = (None, None)
 
         ws_resolution, wa_resolution = self.wind_resolution
-        return _set_resolution(ws_resolution, "s"), _set_resolution(
-            wa_resolution, "a"
+        return (
+            _Resolution.WIND_SPEED.set_resolution(ws_resolution),
+            _Resolution.WIND_ANGLE.set_resolution(wa_resolution)
         )
 
 
