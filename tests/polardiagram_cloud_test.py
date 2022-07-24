690989d9fc13b2cf9c5bdf2dafb312532957dc2f
Loitador41 <julia.garbe@uni-rostock.de>
2022-07-24 15:04:49 +0200

    added some tests for get_slices()

--- tests/polardiagram_cloud_test.py
+++ tests/polardiagram_cloud_test.py
@@ -143,8 +143,38 @@ class PolarDiagramPointCloudTest(unittest.TestCase):
         with self.assertRaises(PolarDiagramException):
             self.pc.get_slices(0)
 
+    def test_get_all_slices(self):
+        ws, wa, bsp = self.pc.get_slices(None)
+        wind_angles = 4 * [np.deg2rad([10, 15, 20, 25])]
+        boat_speeds = [
+            [1.1, 1.5, 1.7, 2.1],
+            [2, 2.4, 2.6, 3],
+            [3, 3.1, 3.5, 3.8],
+            [4, 4.1, 4.4, 4.6]
+        ]
+        self.assertEqual(ws, [2, 4, 6, 8])
+        for i in range(4):
+            with self.subTest(i=i):
+                np.testing.assert_array_equal(wa[i], wind_angles[i])
+                np.testing.assert_array_equal(bsp[i], boat_speeds[i])
+
+    def test_get_slices_interval(self):
+        ws, wa, bsp = self.pc.get_slices(ws=(4, 8))
+        wind_angles = 4 * [np.deg2rad([10, 15, 20, 25])]
+        boat_speeds = [
+            [2, 2.4, 2.6, 3],
+            [3, 3.1, 3.5, 3.8],
+            [3, 3.1, 3.5, 3.8],
+            [4, 4.1, 4.4, 4.6]
+        ]
+        np.testing.assert_array_equal(ws, np.linspace(4, 8, 4))
+        for i in range(4):
+            with self.subTest(i=i):
+                np.testing.assert_array_equal(wa[i], wind_angles[i])
+                np.testing.assert_array_equal(bsp[i], boat_speeds[i])
+
     def test_get_slices_list(self):
-        ws, wa, bsp = self.pc.get_slices([4, 8])
+        ws, wa, bsp = self.pc.get_slices(ws=[4, 8])
         self.assertEqual(ws, [4, 8])
         self.assertEqual(type(wa), list)
         self.assertEqual(type(bsp), list)
@@ -155,6 +185,64 @@ class PolarDiagramPointCloudTest(unittest.TestCase):
         np.testing.assert_array_equal(bsp[0], np.array([2, 2.4, 2.6, 3]))
         np.testing.assert_array_equal(bsp[1], np.array([4, 4.1, 4.4, 4.6]))
 
+    def test_get_slices_mixed_iterable_list(self):
+        ws, wa, bsp = self.pc.get_slices(ws=[(4, 8), 2])
+        self.assertEqual(ws, [6, 2])
+        sorted_angles = sorted(np.concatenate((np.deg2rad([10, 15, 20, 25]),
+                                               np.deg2rad([10, 15, 20, 25]),
+                                               np.deg2rad([10, 15, 20, 25]))))
+        wind_angles = [sorted_angles,
+                       np.deg2rad([10, 15, 20, 25])]
+        boat_speeds = [
+            [2, 3, 4, 2.4, 3.1, 4.1, 2.6, 3.5, 4.4, 3, 3.8, 4.6],
+            [1.1, 1.5, 1.7, 2.1]
+        ]
+        for i in range(2):
+            with self.subTest(i=i):
+                np.testing.assert_array_equal(wa[i], wind_angles[i])
+                np.testing.assert_array_equal(bsp[i], boat_speeds[i])
+
+    def test_get_slices_mixed_iterable_tuple(self):
+        ws, wa, bsp = self.pc.get_slices(ws=((4, 8), 2))
+        self.assertEqual(ws, [6, 2])
+        sorted_angles = sorted(np.concatenate((np.deg2rad([10, 15, 20, 25]),
+                                               np.deg2rad([10, 15, 20, 25]),
+                                               np.deg2rad([10, 15, 20, 25]))))
+        wind_angles = [sorted_angles,
+                       np.deg2rad([10, 15, 20, 25])]
+        boat_speeds = [
+            [2, 3, 4, 2.4, 3.1, 4.1, 2.6, 3.5, 4.4, 3, 3.8, 4.6],
+            [1.1, 1.5, 1.7, 2.1]
+        ]
+        for i in range(2):
+            with self.subTest(i=i):
+                np.testing.assert_array_equal(wa[i], wind_angles[i])
+                np.testing.assert_array_equal(bsp[i], boat_speeds[i])
+
+    def test_get_slices_mixed_iterable_set(self):
+        ws, wa, bsp = self.pc.get_slices(ws={(4, 8), 2})
+        sorted_angles = sorted(np.concatenate((np.deg2rad([10, 15, 20, 25]),
+                                               np.deg2rad([10, 15, 20, 25]),
+                                               np.deg2rad([10, 15, 20, 25]))))
+        wind_angles = [sorted_angles,
+                       np.deg2rad([10, 15, 20, 25])]
+        boat_speeds = [
+            [2, 3, 4, 2.4, 3.1, 4.1, 2.6, 3.5, 4.4, 3, 3.8, 4.6],
+            [1.1, 1.5, 1.7, 2.1]
+        ]
+        for i, elem in enumerate({(4, 8), 2}):
+            with self.subTest(i=i):
+                if i == 0 and elem == (4, 8):
+                    self.assertEqual(ws, [6, 2])
+                else:
+                    self.assertEqual(ws, [2, 6])
+                if elem == (4, 8):
+                    np.testing.assert_array_equal(wa[i], wind_angles[0])
+                    np.testing.assert_array_equal(bsp[i], boat_speeds[0])
+                else:
+                    np.testing.assert_array_equal(wa[i], wind_angles[1])
+                    np.testing.assert_array_equal(bsp[i], boat_speeds[1])
+
     def test_get_slices_exception_empty(self):
         with self.assertRaises(PolarDiagramException):
             self.pc.get_slices([])
@@ -164,7 +252,7 @@ class PolarDiagramPointCloudTest(unittest.TestCase):
             self.pc.get_slices([0, 2])
 
     def test_get_slices_range(self):
-        ws, wa, bsp = self.pc.get_slices((3, 9))
+        ws, wa, bsp = self.pc.get_slices(ws=(3, 9))
         self.assertEqual(ws, [3, 4.2, 5.4, 6.6, 7.8, 9])
         self.assertEqual(type(wa), list)
         self.assertEqual(type(bsp), list)
@@ -197,6 +285,107 @@ class PolarDiagramPointCloudTest(unittest.TestCase):
     def test_get_slices_range_empty_interval(self):
         with self.assertRaises(PolarDiagramException):
             self.pc.get_slices((1, 0))
+
+    def test_get_slices_n_steps(self):
+        ws, wa, bsp = self.pc.get_slices(ws=(4, 8), n_steps=3)
+        self.assertEqual(ws, [4, 6, 8])
+        boat_speeds = [
+            [2, 2.4, 2.6, 3],
+            [3, 3.1, 3.5, 3.8],
+            [4, 4.1, 4.4, 4.6]
+        ]
+        for i in range(3):
+            with self.subTest(i=i):
+                np.testing.assert_array_equal(wa[i], np.deg2rad([10, 15, 20, 25]))
+                np.testing.assert_array_equal(bsp[i], boat_speeds[i])
+
+    def test_get_slices_range_single_ws(self):
+        ws, wa, bsp = self.pc.get_slices(ws=4, range_=2)
+        self.assertEqual(ws, [4])
+        sorted_wind_angles = sorted(np.deg2rad(np.concatenate((self.pc.wind_angles,
+                                                               self.pc.wind_angles,
+                                                               self.pc.wind_angles))))
+        boat_speeds = [1.1, 2, 3, 1.5, 2.4, 3.1, 1.7, 2.6, 3.5, 2.1, 3, 3.8]
+        np.testing.assert_array_equal(np.asarray(wa).flat, sorted_wind_angles)
+        np.testing.assert_array_equal(np.asarray(bsp).flat, boat_speeds)
+
+    def test_get_slices_big_range_(self):
+        ws, wa, bsp = self.pc.get_slices(ws=4, range_=100)
+        self.assertEqual(ws, [4])
+        sorted_wind_angles = sorted(np.deg2rad(np.concatenate((self.pc.wind_angles,
+                                                               self.pc.wind_angles,
+                                                               self.pc.wind_angles,
+                                                               self.pc.wind_angles))))
+        boat_speeds = [1.1, 2, 3, 4, 1.5, 2.4, 3.1, 4.1, 1.7, 2.6, 3.5, 4.4, 2.1, 3, 3.8, 4.6]
+        np.testing.assert_array_equal(np.asarray(wa).flat, sorted_wind_angles)
+        np.testing.assert_array_equal(np.asarray(bsp).flat, boat_speeds)
+
+    def test_get_slices_range_mixed_list(self):
+        pd = self.big_pc
+        ws, wa, bsp = pd.get_slices(ws=[(14, 20), 8], range_=2)
+        np.testing.assert_array_equal(ws, [17, 8])
+        sorted_wind_angles = sorted(np.deg2rad(np.concatenate((pd.wind_angles,
+                                                               pd.wind_angles,
+                                                               pd.wind_angles))))
+        boat_speeds = [
+            [5.47, 5.81, 5.66, 5.67, 6.17, 5.94, 5.95, 6.86, 6.27, 7.35, 6.7, 6.49,
+             7.48, 6.79, 8.76, 7.32, 6.62, 9.74, 8.34, 6.77, 6.22, 6.22, 5.78, 7.32],
+            [3.74, 4.96, 4.48, 3.98, 5.18, 4.73, 4.16, 5.35, 4.93, 5.64, 5.19, 4.35,
+             5.22, 4.39, 5.68, 5.11, 4.23, 5.58, 5.33, 4.64, 3.72, 4.1, 3.21, 4.87]
+        ]
+        for i in range(2):
+            with self.subTest(i=i):
+                np.testing.assert_array_equal(wa[i], sorted_wind_angles)
+                np.testing.assert_array_equal(bsp[i], boat_speeds[i])
+
+    def test_get_slices_range_mixed_tuple(self):
+        pd = self.big_pc
+        ws, wa, bsp = pd.get_slices(ws=((14, 20), 8), range_=2)
+        np.testing.assert_array_equal(ws, [17, 8])
+        sorted_wind_angles = sorted(np.deg2rad(np.concatenate((pd.wind_angles,
+                                                               pd.wind_angles,
+                                                               pd.wind_angles))))
+        boat_speeds = [
+            [5.47, 5.81, 5.66, 5.67, 6.17, 5.94, 5.95, 6.86, 6.27, 7.35, 6.7, 6.49,
+             7.48, 6.79, 8.76, 7.32, 6.62, 9.74, 8.34, 6.77, 6.22, 6.22, 5.78, 7.32],
+            [3.74, 4.96, 4.48, 3.98, 5.18, 4.73, 4.16, 5.35, 4.93, 5.64, 5.19, 4.35,
+             5.22, 4.39, 5.68, 5.11, 4.23, 5.58, 5.33, 4.64, 3.72, 4.1, 3.21, 4.87]
+        ]
+        for i in range(2):
+            with self.subTest(i=i):
+                np.testing.assert_array_equal(wa[i], sorted_wind_angles)
+                np.testing.assert_array_equal(bsp[i], boat_speeds[i])
+
+    def test_get_slices_range_mixed_set(self):
+        pd = self.big_pc
+        ws, wa, bsp = pd.get_slices(ws={(14, 20), 8}, range_=2)
+        sorted_wind_angles = sorted(np.deg2rad(np.concatenate((pd.wind_angles,
+                                                               pd.wind_angles,
+                                                               pd.wind_angles))))
+        boat_speeds = [
+            [5.47, 5.81, 5.66, 5.67, 6.17, 5.94, 5.95, 6.86, 6.27, 7.35, 6.7, 6.49,
+             7.48, 6.79, 8.76, 7.32, 6.62, 9.74, 8.34, 6.77, 6.22, 6.22, 5.78, 7.32],
+            [3.74, 4.96, 4.48, 3.98, 5.18, 4.73, 4.16, 5.35, 4.93, 5.64, 5.19, 4.35,
+             5.22, 4.39, 5.68, 5.11, 4.23, 5.58, 5.33, 4.64, 3.72, 4.1, 3.21, 4.87]
+        ]
+        for i, elem in enumerate({(14, 20), 8}):
+            np.testing.assert_array_equal(wa[i], sorted_wind_angles)
+            if i == 0 and elem == (14, 20):
+                np.testing.assert_array_equal(ws, [17, 8])
+            else:
+                np.testing.assert_array_equal(ws, [8, 17])
+            if elem == (14, 20):
+                np.testing.assert_array_equal(bsp[i], boat_speeds[0])
+            else:
+                np.testing.assert_array_equal(bsp[i], boat_speeds[1])
+
+    def test_get_slices_exception_nonpositive_range_(self):
+        with self.subTest(i=0):
+            with self.assertRaises(PolarDiagramException):
+                self.pc.get_slices(ws=4, range_=0)
+        with self.subTest(i=1):
+            with self.assertRaises(PolarDiagramException):
+                self.pc.get_slices(ws=4, range_=-20)
 
     def test_plot_polar(self):
         self.pc.plot_polar()
@@ -277,14 +466,11 @@ class PolarDiagramPointCloudTest(unittest.TestCase):
 
     def test_plot_polar_big_range_(self):
         self.pc.plot_polar(ws=4, range_=100)
-        sorted_wind_angles = sorted(np.deg2rad(np.concatenate((self.pc.wind_angles,
-                                                               self.pc.wind_angles,
-                                                               self.pc.wind_angles,
-                                                               self.pc.wind_angles))))
+        ws, wa, bsp = self.pc.get_slices(ws=4, range_=100)
         x_plot = plt.gca().lines[0].get_xdata()
         y_plot = plt.gca().lines[0].get_ydata()
-        np.testing.assert_array_equal(x_plot, sorted_wind_angles)
-        np.testing.assert_array_equal(y_plot, [1.1, 2, 3, 4, 1.5, 2.4, 3.1, 4.1, 1.7, 2.6, 3.5, 4.4, 2.1, 3, 3.8, 4.6])
+        np.testing.assert_array_equal(x_plot, np.asarray(wa).flat)
+        np.testing.assert_array_equal(y_plot, np.asarray(bsp).flat)
 
     def test_plot_polar_range_mixed_list(self):
         pd = self.big_pc
