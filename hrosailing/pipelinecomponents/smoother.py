ec92cfac0adcdcef61e2df1ecb3e0ac5ea501bee
DESKTOP-9273532\USER <robert.schueler1989@gmail.com>
2022-11-11 15:24:37 +0100

    overhauled AffineSmoother

--- hrosailing/pipelinecomponents/smoother.py
+++ hrosailing/pipelinecomponents/smoother.py
@@ -7,7 +7,7 @@ Also contains predefined and usable smoothers:
 - `LazySmoother`,
 - `AffineSmoother`.
 """
-
+import datetime
 from abc import ABC, abstractmethod
 
 
@@ -59,7 +59,15 @@ class AffineSmoother(Smoother):
     intervals is used to replace the measured data.
     Then, data points in such an interval are replaced using affine
     interpolation.
+
+    Parameter
+    ---------
+    timespan: datetime.timedelta
+        Amount of time before the actual value of an interval is assumed to be the real value.
     """
+
+    def __init__(self, timespan = datetime.timedelta(seconds = 30)):
+        self._timespan = timespan.total_seconds()
 
     def smooth(self, data):
         """
@@ -82,28 +90,62 @@ class AffineSmoother(Smoother):
         i_start = 0
         x_lb = xs[0]
         y_lb = ys[0]
+        interval_bounds_x = []
+        interval_bounds_y = []
         for i, (x, x_after, y, y_after) in enumerate(
                 zip(xs, xs[1:], ys, ys[1:])):
-            if y != y_after or i == len(xs) - 2:
-                y_ub = 1 / 2 * (y + y_after)
-                x_ub = 1 / 2 * (x + x_after)
-                mid_pt = 1 / 2 * (x_lb + x_ub)
+            if i == len(ys) - 2:
+                interval_bounds_x.append((x_lb, x_after))
+                interval_bounds_y.append(y)
+                break
+            if y == y_after:
+                continue
+            interval_bounds_x.extend([(x_lb, 1/2*(x + x_after))])
+            interval_bounds_y.append(y_lb)
+            x_lb = 1/2*(x + x_after)
+            y_lb = y_after
 
-                # affine approximation of sample points
-                for j, x_sample in enumerate(xs[i_start: i + 1]):
-                    if x_sample <= mid_pt:
-                        try:
-                            lamb = (x_sample - x_lb) / (mid_pt - x_lb)
-                        except ZeroDivisionError:
-                            lamb = 0
-                        data[key][i_start + j] = lamb * y + (1 - lamb) * y_lb
-                    else:
-                        try:
-                            lamb = (x_sample - mid_pt) / (x_ub - mid_pt)
-                        except ZeroDivisionError:
-                            lamb = 0
-                        data[key][i_start + j] = lamb * y_ub + (1 - lamb) * y
-                i_start = i + 1
-                x_lb = x_ub
-                y_lb = y_ub
+        approx_intervals_x = []
+        approx_intervals_y = []
+
+        y_before = ys[0]
+        # for (x_lb, x_ub), y, y_after in zip(
+        #         interval_bounds_x, interval_bounds_y, interval_bounds_y[1:] + [ys[-1]]
+        # ):
+        for (x_lb, x_ub), y, y_after in zip(
+                interval_bounds_x, interval_bounds_y, interval_bounds_y[1:] + [ys[-1]]
+        ):
+            if x_ub - x_lb < 2*self._timespan:
+                midpt = 1/2*(x_ub + x_lb)
+                approx_intervals_x.extend([(x_lb, midpt), (midpt, x_ub)])
+                approx_intervals_y.extend([(1/2*(y_before + y), y), (y, 1/2*(y_after + y))])
+            else:
+                lb_ref = x_lb + self._timespan
+                ub_ref = x_ub - self._timespan
+                approx_intervals_x.extend([
+                    (x_lb, lb_ref), (lb_ref, ub_ref), (ub_ref, x_ub)
+                ])
+                approx_intervals_y.extend([
+                    (1/2*(y_before + y), y), (y, y), (y, 1/2*(y_after + y))
+                ])
+            y_before = y
+
+        x_lb, x_ub = approx_intervals_x[0]
+        y_lb, y_ub = approx_intervals_y[0]
+        interval_idx = 0
+        for i, x in enumerate(xs):
+            if x > x_ub:
+                interval_idx += 1
+                try:
+                    x_lb, x_ub = approx_intervals_x[interval_idx]
+                    y_lb, y_ub = approx_intervals_y[interval_idx]
+                except IndexError:
+                    break
+
+            try:
+                mu = (x - x_lb)/(x_ub - x_lb)
+                data[key][i] = mu*y_ub + (1-mu)*y_lb
+            except ZeroDivisionError:
+                data[key][i] = y_ub
+
         return data
