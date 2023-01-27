4ac84deb2f8de96486ddbfbe4b79e52b796e72ad
DESKTOP-9273532\USER <robert.schueler1989@gmail.com>
2023-01-27 08:20:18 +0100

    refactored info logic in plotting

--- hrosailing/plotting/projections.py
+++ hrosailing/plotting/projections.py
@@ -61,29 +61,13 @@ class HROPolar(PolarAxes):
             wa = np.deg2rad(wa)
             super().plot(wa, bsp, **kwargs)
 
-
     def _plot_with_info(self, slices, info, **kwargs):
         for slice, info_ in zip(slices, info):
             ws, wa, bsp = slice
             wa = np.deg2rad(wa)
-            intervals = self._get_info_intervals(info_)
-            wa = self._merge(wa, intervals)
-            print(wa)
-            print(bsp)
-            bsp = self._merge(bsp, intervals)
+            wa, bsp = _alter_with_info(wa, bsp, info_)
             super().plot(wa, bsp, **kwargs)
 
-    def _get_info_intervals(self, info_):
-        intervals = {}
-        for j, entry in enumerate(info_):
-            if entry not in intervals:
-                intervals[entry] = []
-            intervals[entry].append(j)
-        return intervals.values()
-
-    def _merge(self, wa, intervals):
-        wa_in_intervals = [np.concatenate([wa[interval], [np.NAN]]) for interval in intervals]
-        return np.concatenate(wa_in_intervals)[:-1]
 
 class HROFlat(Axes):
     name = "hro flat"
@@ -207,6 +191,26 @@ register_projection(HROPolar)
 register_projection(HROFlat)
 register_projection(HROColorGradient)
 register_projection(HRO3D)
+
+
+def _get_info_intervals(info_):
+    intervals = {}
+    for j, entry in enumerate(info_):
+        if entry not in intervals:
+            intervals[entry] = []
+        intervals[entry].append(j)
+    return intervals.values()
+
+
+def _merge(wa, intervals):
+    wa_in_intervals = [np.concatenate([wa[interval], [np.NAN]]) for interval in intervals]
+    return np.concatenate(wa_in_intervals)[:-1]
+
+def _alter_with_info(wa, bsp, info_):
+    intervals = _get_info_intervals(info_)
+    wa = _merge(wa, intervals)
+    bsp = _merge(bsp, intervals)
+    return wa, bsp
 
 def _check_for_lines(wa):
     return wa.ndim == 1
