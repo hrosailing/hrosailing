bf7bdab0588abc7adcd4969e4fc3985740c0bd1e
Robert Schueler <robert.schueler1989@gmail.com>
2023-02-22 15:35:21 +0100

    init of pipeline

--- hrosailing/pipeline/__init__.py
+++ hrosailing/pipeline/__init__.py
@@ -1,1 +1,13 @@
-from hrosailing.pipeline.pipeline import PipelineOutput, PolarPipeline
+"""
+Contains a modular preprocessing pipeline in order to generate polar diagrams
+from raw data as well as several high level components for said pipeline.
+"""
+
+from .pipeline import PipelineOutput, PolarPipeline
+from .expander import Expander, LazyExpander, WeatherExpander
+from .extensions import (
+    PipelineExtension, TableExtension, PointcloudExtension, CurveExtension
+)
+from .quality_assurance import (
+    QualityAssurance, ComformingQualityAssurance, MinimalQualityAssurance
+)
