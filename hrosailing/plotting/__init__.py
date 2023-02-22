8bf3cdbb85abf02186b5a2d00976673f0a5390a6
DESKTOP-9273532\USER <robert.schueler1989@gmail.com>
2023-02-22 07:12:17 +0100

    imported one-shot plot methods

--- hrosailing/plotting/__init__.py
+++ hrosailing/plotting/__init__.py
@@ -4,3 +4,8 @@ Currently, plots of `PolarDiagram` instances are supported.
 """
 
 import hrosailing.plotting.projections
+
+from hrosailing.plotting.projections import (
+    plot_flat, plot_polar, plot_color_gradient, plot_3d,
+    scatter_flat, scatter_polar, scatter_3d
+)
