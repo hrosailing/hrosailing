5c490145a3308ad08b2ea5a87878c7facdff7be1
DESKTOP-9273532\USER <robert.schueler1989@gmail.com>
2023-03-23 13:18:01 +0100

    added more tests for private methods

---
+++ tests/test_plotting/test_projections/test_methods/test_set_polar_axis.py
@@ -0,0 +1,9 @@
+import unittest
+import matplotlib.pyplot as plt
+
+from hrosailing.plotting.projections import _set_polar_axis
+
+class TestSetPolarAxis(unittest.TestCase):
+    def test_execution(self):
+        # Execution Test
+        _set_polar_axis(plt.subplot(projection="polar"))
