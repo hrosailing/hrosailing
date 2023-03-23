052d35d7e1a3c03edfbb7d0d40e226c8df2b548e
DESKTOP-9273532\USER <robert.schueler1989@gmail.com>
2023-03-23 13:01:44 +0100

    added tests for _configure_axis

--- tests/test_plotting/test_projections/test_methods/test_configure_axis.py
+++ tests/test_plotting/test_projections/test_methods/test_configure_axis.py
@@ -15,3 +15,24 @@ class TestConfigureAxis(unittest.TestCase):
             None,
             marker="H"
         )
+
+    def test_with_show_legend_without_keywords(self):
+        # Execution test with `show_legend=True`, no keywords
+        _configure_axes(
+            plt.subplot(),
+            [1, 2, 3],
+            ("green", "red"),
+            False,
+            None
+        )
+
+    def test_with_show_legend_with_keywords(self):
+        # Execution test with `show_legend=True` and keywords
+        _configure_axes(
+            plt.subplot(),
+            [1, 2, 3],
+            ("green", "red"),
+            False,
+            None,
+            marker="H"
+        )
