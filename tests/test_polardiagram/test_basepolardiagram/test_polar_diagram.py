8a832f1085c6e1a7eea9af21820b32452c12f113
Robert Schueler <robert.schueler1989@gmail.com>
2023-03-28 08:41:15 +0200

    tests and debugging for `PolarDiagram._get_windspeeds`

--- tests/test_polardiagram/test_basepolardiagram/test_polar_diagram.py
+++ tests/test_polardiagram/test_basepolardiagram/test_polar_diagram.py
@@ -23,10 +23,10 @@ class DummyPolarDiagram(PolarDiagram):
 
     @property
     def default_slices(self):
-        pass
+        return [1, 2, 3]
 
     def ws_to_slices(self, ws, **kwargs):
-        return ws
+        return np.array([[1],[1],[1]])
 
 
 
@@ -45,7 +45,7 @@ class TestPolarDiagram(unittest.TestCase):
         np.testing.assert_array_equal(ws, np.array([1, 1.5, 2, 2.5, 3]))
         np.testing.assert_array_equal(
             slices,
-            np.array([1, 1.5, 2, 2.5, 3])
+            np.array([[1],[1],[1]])
         )
 
     def test_get_slices_with_full_info(self):
@@ -59,6 +59,41 @@ class TestPolarDiagram(unittest.TestCase):
         np.testing.assert_array_equal(ws, np.array([1, 1.5, 2, 2.5, 3]))
         np.testing.assert_array_equal(
             slices,
-            np.array([1, 1.5, 2, 2.5, 3])
+            np.array([[1],[1],[1]])
         )
         self.assertIsNone(info)
+
+    def test_get_windspeeds_ws_None(self):
+        # Input/Output
+        result = self.pd._get_windspeeds(None, None)
+        np.testing.assert_array_equal(result, [1, 2, 3])
+
+    def test_get_windspeeds_ws_float(self):
+        # Input/Output
+        result = self.pd._get_windspeeds(3.5, None)
+        np.testing.assert_array_equal(result, [3.5])
+
+    def test_get_windspeeds_ws_not_number_or_iterable(self):
+        # Exception test
+        with self.assertRaises(TypeError):
+            self.pd._get_windspeeds(self.pd, None)
+
+    def test_get_windspeeds_not_all_numbers(self):
+        # Exception test
+        with self.assertRaises(TypeError):
+            self.pd._get_windspeeds(["hallo", 1, 2, self.pd], None)
+
+    def test_get_windspeeds_nsteps_negative(self):
+        # Exception test
+        with self.assertRaises(ValueError):
+            self.pd._get_windspeeds(None, -1)
+
+    def test_get_windspeeds_regular_input(self):
+        # Input/Output test
+        result = self.pd._get_windspeeds([1, 2, 3], 1)
+        np.testing.assert_array_equal(
+            result,
+            [1, 1.5, 2, 2.5, 3]
+        )
+
+    
